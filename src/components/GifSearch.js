a10aa980ecb761a6158be61641f2e23821ea6c91
Bloom <bloomtauta@gmail.com>
2023-01-22 15:33:19 +0300

    Completed assignment

---
+++ src/components/GifSearch.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function GifSearch() {
+    return (
+        <div className="container-fluid">
+            <form className="container">
+                <input className="form-control me-1" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default GifSearch
